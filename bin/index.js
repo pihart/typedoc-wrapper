e4284991ae281db3dc00d9e8e03d858964da8108
pihart <avi.mehra1@gmail.com>
2021-02-03 20:31:32 -0800

    Log to stderr and stdout
    Resolves https://github.com/pihart/typedoc-wrapper/issues/7

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 const run = require("../src");
-const { argv, stdout } = process;
-const [, , filePath, ...rest] = argv;
+const [, , filePath, ...rest] = process.argv;
 
 if (filePath === undefined) {
-  stdout.write("Usage: tdx path-to-file [...typedoc options]");
+  process.stdout.write("Usage: tdx path-to-file [...typedoc options]");
   process.exitCode = 2;
 } else {
-  run(filePath, { postArgs: rest });
-  process.exitCode = 0;
+  run(filePath, { postArgs: rest })
+    .then(({ stdout, stderr }) => {
+      process.stdout.write(stdout);
+      process.stderr.write(stderr);
+      process.exitCode = 0;
+    })
+    .catch((error) => {
+      process.stderr.write(error.message);
+      process.exitCode = 1;
+    });
 }
